9c1b5188abfd906e7ed7ebe3885afd61c46ba45f
maria2080377 <masha900p@gmail.com>
2025-09-26 20:31:39 +0300

    Add files via upload

---
+++ laba.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream> 
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	int a[10];
+	int suma = 0;
+
+	for (int i = 0; i < 10; i++) {
+		cin >> a[i];
+	}
+
+	int mini = a[0];
+
+	for (int i = 0; i < 10; i++) {
+		cout << a[i] << " ";
+		suma += a[i];
+
+		if (a[i] < mini) {
+			mini = a[i];
+		}
+	}
+	cout << endl;
+	cout<< "Сумма: "<< suma << endl;
+	cout << "Минимальное значение: " << mini << endl;
+
+	for (int i = 1; i < 10; i++) {
+		int k = a[i];
+		int j = i - 1;
+
+		while (j >= 0 && a[j] > k) {
+			a[j + 1] = a[j];
+			j--;
+		}
+		a[j + 1] = k;
+	}
+
+	for (int i = 0; i < 10; i++) {
+		cout << a[i] << " ";
+	}
+	return 0;
+}
---
+++ laba1.cpp
@@ -0,0 +1,39 @@
+﻿//#include <iostream> 
+//using namespace std;
+//int main() {
+//	setlocale(LC_ALL, "RU");
+//
+//	float a;
+//	cout << "Введите температуру: ";
+//	cin >> a;
+//
+//	if (a < 0) {
+//		cout << "Наденьте зимнюю одежду" << endl;
+//	}
+//	else if (a >= 0 && a <= 10) {
+//		cout << "Наденьте тёплую одежду" << endl;
+//	}
+//	else if (a >= 11 && a <= 20) {
+//		cout << "Наденьте лёгкую одежду" << endl;
+//	}
+//	else {
+//		cout << "Наденьте летнюю одежду" << endl;
+//	}
+//	return 0;
+//}
+
+#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	int a;
+	int res = 0;
+	cout << "Введите количество поездок: ";
+	cin >> a;
+	for (int i = 0; i < a; i++) {
+		res += 5;
+	}
+	cout << "Общая стоимость: " << res;
+	return 0;
+}
